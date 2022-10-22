f55fccddc91f6b2b6f603bbeb9e6875e8cb49eca
Muhammad Zohaib <zohaib87@live.com>
2022-10-22 19:28:41 +0500

    commented out registration hooks

--- includes/class-custom-post-types.php
+++ includes/class-custom-post-types.php
@@ -1,72 +1,72 @@
-<?php 
+<?php
 /**
  * Class for adding custom post types.
  *
  * @package Xe Plugin
  */
 
-if (!class_exists('Xe_Plugin_CustomPostTypes')) :
+if (!class_exists('Xe_Plugin_CustomPostTypes')) {
 
-class Xe_Plugin_CustomPostTypes {
+  class Xe_Plugin_CustomPostTypes {
 
-  function __construct() {
+    function __construct() {
 
-    add_action( 'init', array($this, 'custom_post_types') );
-    register_activation_hook( __FILE__, array($this, 'rewrite_flush') );
+      // add_action( 'init', array($this, 'custom_post_types') );
+      // register_activation_hook( __FILE__, array($this, 'rewrite_flush') );
+
+    }
+
+    protected function portfolio() {
+
+      $labels = array(
+        'name'               => 'Portfolio',
+        'singular_name'      => 'Portfolio',
+        'menu_name'          => 'Portfolio',
+        'name_admin_bar'     => 'Portfolio',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Portfolio',
+        'new_item'           => 'New Portfolio',
+        'edit_item'          => 'Edit Portfolio',
+        'view_item'          => 'View Portfolio',
+        'all_items'          => 'All Portfolios',
+        'search_items'       => 'Search Portfolios',
+        'parent_item_colon'  => 'Parent Portfolios:',
+        'not_found'          => 'No Portfolios found.',
+        'not_found_in_trash' => 'No Portfolios found in Trash.',
+      );
+
+      $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'menu_icon'          => 'dashicons-portfolio',
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'xe-portfolio' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'supports'           => array( 'title', 'editor', 'thumbnail', 'comments', 'revisions' ),
+      );
+      return register_post_type( 'xe-portfolio', $args );
+
+    }
+
+    public function custom_post_types() {
+
+      $this->portfolio();
+
+    }
+
+    public function rewrite_flush() {
+
+      $this->custom_post_types();
+      flush_rewrite_rules();
+
+    }
 
   }
-
-  protected function portfolio() {
-
-    $labels = array(
-      'name'               => 'Portfolio',
-      'singular_name'      => 'Portfolio',
-      'menu_name'          => 'Portfolio',
-      'name_admin_bar'     => 'Portfolio',
-      'add_new'            => 'Add New',
-      'add_new_item'       => 'Add New Portfolio',
-      'new_item'           => 'New Portfolio',
-      'edit_item'          => 'Edit Portfolio',
-      'view_item'          => 'View Portfolio',
-      'all_items'          => 'All Portfolios',
-      'search_items'       => 'Search Portfolios',
-      'parent_item_colon'  => 'Parent Portfolios:',
-      'not_found'          => 'No Portfolios found.',
-      'not_found_in_trash' => 'No Portfolios found in Trash.',
-    );
-    
-    $args = array(
-      'labels'             => $labels,
-      'public'             => true,
-      'publicly_queryable' => true,
-      'show_ui'            => true,
-      'show_in_menu'       => true,
-      'menu_icon'          => 'dashicons-portfolio',
-      'query_var'          => true,
-      'rewrite'            => array( 'slug' => 'xe-portfolio' ),
-      'capability_type'    => 'post',
-      'has_archive'        => true,
-      'hierarchical'       => false,
-      'supports'           => array( 'title', 'editor', 'thumbnail', 'comments', 'revisions' ),
-    );
-    return register_post_type( 'xe-portfolio', $args );
-
-  }
-
-  public function custom_post_types() {
-
-  	$this->portfolio();
-
-  }
-
-  public function rewrite_flush() {
-
-    $this->custom_post_types();
-    flush_rewrite_rules();
-
-  }
+  new Xe_Plugin_CustomPostTypes();
 
 }
-new Xe_Plugin_CustomPostTypes();
-
-endif;
--- includes/class-custom-taxonomies.php
+++ includes/class-custom-taxonomies.php
@@ -1,56 +1,56 @@
-<?php 
+<?php
 /**
  * Class for adding custom taxonomies.
  *
  * @package Xe Plugin
  */
 
-if (!class_exists('Xe_Plugin_CustomTaxonomies')) :
+if (!class_exists('Xe_Plugin_CustomTaxonomies')) {
 
-class Xe_Plugin_CustomTaxonomies {
+  class Xe_Plugin_CustomTaxonomies {
 
-  function __construct() {
+    function __construct() {
 
-  	add_action( 'init', array($this, 'register_taxonomies'), 0 );
+      // add_action( 'init', array($this, 'register_taxonomies'), 0 );
+
+    }
+
+    protected function portfolio_categories() {
+
+      $labels = array(
+        'name'              => 'Categories',
+        'singular_name'     => 'Category',
+        'search_items'      => 'Search Categories',
+        'all_items'         => 'All Categories',
+        'parent_item'       => 'Parent Category',
+        'parent_item_colon' => 'Parent Category:',
+        'edit_item'         => 'Edit Category',
+        'update_item'       => 'Update Category',
+        'add_new_item'      => 'Add New Category',
+        'new_item_name'     => 'New Category Name',
+        'menu_name'         => 'Categories',
+      );
+
+      $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'xe-portfolio-categories' ),
+      );
+
+      return $args;
+
+    }
+
+    public function register_taxonomies() {
+
+      register_taxonomy( 'xe-portfolio-categories', array('xe-portfolio'), $this->portfolio_categories() );
+
+    }
 
   }
-
-  protected function portfolio_categories() {
-	
-    $labels = array(
-      'name'              => 'Categories',
-      'singular_name'     => 'Category',
-      'search_items'      => 'Search Categories',
-      'all_items'         => 'All Categories',
-      'parent_item'       => 'Parent Category',
-      'parent_item_colon' => 'Parent Category:',
-      'edit_item'         => 'Edit Category',
-      'update_item'       => 'Update Category',
-      'add_new_item'      => 'Add New Category',
-      'new_item_name'     => 'New Category Name',
-      'menu_name'         => 'Categories',
-    );
-
-    $args = array(
-      'hierarchical'      => true,
-      'labels'            => $labels,
-      'show_ui'           => true,
-      'show_admin_column' => true,
-      'query_var'         => true,
-      'rewrite'           => array( 'slug' => 'xe-portfolio-categories' ),
-    );
-
-    return $args;
-
-	}
-
-	public function register_taxonomies() {
- 
-		register_taxonomy( 'xe-portfolio-categories', array('xe-portfolio'), $this->portfolio_categories() );
-
-	}
+  new Xe_Plugin_CustomTaxonomies();
 
 }
-new Xe_Plugin_CustomTaxonomies();
-
-endif;
