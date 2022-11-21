f88d5df8ea4c1fb025c31b6d5319ac7ac322f8f5
Muhammad Zohaib <zohaib87@live.com>
2022-11-21 14:30:19 +0500

    Added tabs view for plugin options

--- helpers/class-views.php
+++ helpers/class-views.php
@@ -11,14 +11,22 @@ if (!class_exists('Xe_Plugin_Views')) {
 
   class Xe_Plugin_Views {
 
-    /*--------------------------------------------------------------
-    # Sample
-    --------------------------------------------------------------*/
-    public static function sample() {
+    /**
+     * # Tabs for Plugin Options
+     */
+    public static function plugin_option_tabs($tabs) {
 
-      return array(
-        'This is a sample array'
-      );
+      foreach ($tabs as $tab) {
+
+        $title = $tab[0];
+        $param = $tab[1];
+        $active_tab = $tab[2];
+
+        ?>
+          <a href="?page=<?php echo $_GET['page']; ?>&tab=<?php echo esc_attr($param); ?>" class="nav-tab <?php echo ($active_tab == $param) ? 'nav-tab-active' : ''; ?>"><?php echo esc_html($title); ?></a>
+        <?php
+
+      }
 
     }
 
