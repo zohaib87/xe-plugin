740dfbc5e2ee68677d2d776fa9cfa5fefdad7c37
Muhammad Zohaib <zohaib87@live.com>
2023-03-16 18:12:22 +0500

    Files renamed according to WP Coding Standards

--- xe-plugin.php
+++ xe-plugin.php
@@ -17,61 +17,61 @@ require 'helpers/functions.php';
 /**
  * Plugin setup functions and definitions.
  */
-require _xe_plugin_directory() . '/includes/setup.php';
+require _xe_plugin_directory() . '/includes/class-setup.php';
 
 /**
  * Object for containing default values.
  */
-require _xe_plugin_directory() . '/helpers/defaults.php';
+require _xe_plugin_directory() . '/helpers/class-defaults.php';
 
 /**
  * Class that holds helper methods.
  */
-require _xe_plugin_directory() . '/helpers/helpers.php';
+require _xe_plugin_directory() . '/helpers/class-helpers.php';
 
 /**
  * Class for reusable sections.
  */
-require _xe_plugin_directory() . '/helpers/views.php';
+require _xe_plugin_directory() . '/helpers/class-views.php';
 
 /**
  * Class to get and use plugin options.
  */
-require _xe_plugin_directory() . '/helpers/plugin-options.php';
+require _xe_plugin_directory() . '/helpers/class-plugin-options.php';
 
 /**
  * Enqueue scripts and styles for admin and front end.
  */
-require _xe_plugin_directory() . '/includes/scripts.php';
+require _xe_plugin_directory() . '/includes/class-scripts.php';
 
 /**
  * MetaBoxes
  */
-require _xe_plugin_directory() . '/includes/metaboxes/sample.php';
+require _xe_plugin_directory() . '/includes/metaboxes/class-sample.php';
 
 /**
  * Menu or sub-menu Pages
  */
-require _xe_plugin_directory() . '/includes/menu-pages.php';
-require _xe_plugin_directory() . '/includes/callbacks/plugin-options.php';
+require _xe_plugin_directory() . '/includes/class-menu-pages.php';
+require _xe_plugin_directory() . '/includes/callbacks/class-plugin-options.php';
 
 /**
  * Class for adding custom post types.
  */
-require _xe_plugin_directory() . '/includes/custom-post-types.php';
+require _xe_plugin_directory() . '/includes/class-custom-post-types.php';
 
 /**
  * Class for adding custom taxonomies.
  */
-require _xe_plugin_directory() . '/includes/custom-taxonomies.php';
+require _xe_plugin_directory() . '/includes/class-custom-taxonomies.php';
 
 /**
  * Class for adding custom WooCommerce fields, Tabs and Product types
  */
-// require _xe_plugin_directory() . '/includes/product-backend.php';
-// require _xe_plugin_directory() . '/includes/product-frontend.php';
+// require _xe_plugin_directory() . '/includes/class-product-backend.php';
+// require _xe_plugin_directory() . '/includes/class-product-frontend.php';
 
 /**
  * Shortcodes
  */
-require _xe_plugin_directory() . '/includes/shortcodes/sample.php';
+require _xe_plugin_directory() . '/includes/shortcodes/class-sample.php';
