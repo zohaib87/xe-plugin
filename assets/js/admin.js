97d684afbc573e49481a22d56c78bd580122c0c2
Muhammad Zohaib <zohaib87@live.com>
2024-10-04 01:26:26 +0500

    Fixed some minor bugs

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,8 +1,17 @@
 /*--------------------------------------------------------------
 # Admin Js Start
 --------------------------------------------------------------*/
-( function ($) {
+( function ( $ ) {
 
-	// ....
+	/**
+   * # Active menu fix
+   */
+  if ( xepObj.postType.indexOf( 'xe-plugin-' ) !== -1 && ( xepObj.base == 'post' || xepObj.base == 'edit-tags' ) ) {
+
+    $( '#toplevel_page_xe-plugin-options, #toplevel_page_xe-plugin-options > a.toplevel_page_xe-plugin-options' )
+      .removeClass( 'wp-not-current-submenu' )
+      .addClass( 'wp-has-current-submenu wp-menu-open' );
+
+  }
 
 } )( jQuery );
